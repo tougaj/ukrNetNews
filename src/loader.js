706baca46efa11ef465e430a2f45b7d030dac593
Alex <alex17101969@gmail.com>
2021-08-05 14:20:40 +0300

    Реализована загрузка данных с сайта через прокси-сервер

--- src/loader.js
+++ src/loader.js
@@ -2,13 +2,50 @@
 
 const fetch = require('node-fetch');
 const fs = require('fs');
+const HttpsProxyAgent = require('https-proxy-agent');
+const moment = require('moment');
 
-fetch('https://www.ukr.net/news/dat/main/0/')
-    .then(data => data.json())
-    .then(json => {
-        // let tops = json.tops.slice(0, 2);
-        let tops = json.tops;
-        let sTops = JSON.stringify(tops);
-        fs.writeFileSync('ukr_net_tops.json', sTops)
-    })
-    .catch(error => console.error(error));
+const proxy = 'http://192.168.0.1:3128';
+const proxyAgent = new HttpsProxyAgent(proxy);
+
+const getFileName = () => moment().format('YYYYMMDD_HHmmss') + '.json';
+
+const getNews = (tops) => {
+	const news = tops.map(({ Title, Description, DateCreated, NewsCount }) => ({
+		title: Title,
+		description: Description,
+		created: moment(DateCreated * 1000).toISOString(),
+		count: NewsCount,
+	}));
+	return news;
+};
+
+// 212.42.76.252
+const loadUkrNetNews = (route) =>
+	fetch(`https://www.ukr.net/news/dat/${route}/0/`, {
+		agent: proxyAgent,
+	})
+		.then((data) => data.json())
+		.then((json) => {
+			// let tops = json.tops.slice(0, 2);
+			const tops = json.tops;
+
+			const result = {
+				route,
+				news: getNews(tops),
+			};
+
+			const sResult = JSON.stringify(result, null, '\t');
+			fs.writeFileSync(`output/ukrnet_${route}_${getFileName()}`, sResult);
+			console.log(`${route} loaded`);
+		})
+		.catch((error) => console.error(error));
+
+const loadAllNews = async () => {
+	await Promise.all(['main', 'politics', 'economics', 'criminal', 'world'].map(loadUkrNetNews));
+	console.log('\nAll routes loaded');
+};
+
+loadAllNews();
+
+// console.log(moment(1628158063 * 1000).toISOString());
