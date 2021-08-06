aa930b756d6d92522cf4e631082185acb9e091ad
Alex <alex17101969@gmail.com>
2021-08-06 14:44:57 +0300

    Добавлено создание дистрибутива

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,15 @@
 const gulp = require('gulp');
 const ts = require('gulp-typescript');
-var changed = require('gulp-changed');
-// const webpack = require('webpack-stream');
-// const del = require('del');
+const changed = require('gulp-changed');
 const plumber = require('gulp-plumber');
+const uglify = require('gulp-uglify');
+const del = require('del');
 
 let paths = {
 	scripts: {
 		src: ['src/**/*.ts', '!src/**/*.d.ts'],
-		dest: 'dest',
+		dst: './dst',
+		dist: './dist',
 	},
 };
 
@@ -25,127 +26,27 @@ function typeScripts() {
 		)
 		.pipe(tsProject());
 
-	return tsResult.js.pipe(plumber()).pipe(gulp.dest(paths.scripts.dest));
+	return tsResult.js.pipe(plumber()).pipe(gulp.dest(paths.scripts.dst));
 }
 
-// const sDistDir = './dist';
+const clean = () => del([paths.scripts.dist]);
 
-// const clean = () => del([sDistDir]);
-// const webpackDev = () => runDevWebPack('js/**/*.js', './js/main.js');
-// const webpackProd = () => runProdWebPack('js/**/*.js', './js/main.js', `${sDistDir}/js`);
+const compress = () => {
+	return gulp
+		.src(`${paths.scripts.dst}/loader.js`)
+		.pipe(plumber())
+		.pipe(uglify())
+		.pipe(gulp.dest(paths.scripts.dist));
+};
 
 function watch() {
-	// gulp.watch(paths.scripts.src, gulp.series(typeScripts, webpackDev));
 	gulp.watch(paths.scripts.src, gulp.series(typeScripts));
 }
 
+gulp.task('clean', clean);
+
 gulp.task('ts', typeScripts);
-// gulp.task('webpack', gulp.series(typeScripts, webpackDev));
 
-// gulp.task(
-// 	'build',
-// 	gulp.series(
-// 		clean,
-// 		styles,
-// 		typeScripts,
-// 		gulp.parallel(
-// 			() =>
-// 				gulp
-// 					.src(['./*.html'])
-// 					.pipe(plumber())
-// 					.pipe(replace(/ts=\[\[0000000000\]\]/g, `ts=${new Date().valueOf()}`))
-// 					.pipe(gulp.dest(sDistDir)),
-// 			() =>
-// 				gulp
-// 					.src(['includes/**/*.*'])
-// 					.pipe(plumber())
-// 					.pipe(gulp.dest(`${sDistDir}/includes`)),
-// 			() =>
-// 				gulp
-// 					.src(['img/**/*.*'])
-// 					.pipe(plumber())
-// 					.pipe(gulp.dest(`${sDistDir}/img`)),
-// 			() =>
-// 				gulp
-// 					.src('css/**/*.css')
-// 					.pipe(plumber())
-// 					.pipe(cleanCSS())
-// 					.pipe(gulp.dest(`${sDistDir}/css`)),
-// 			webpackProd
-// 		)
-// 	)
-// );
+gulp.task('default', gulp.series(typeScripts, watch));
 
-// let development = gulp.series(styles, typeScripts, webpackDev, watch);
-let development = gulp.series(typeScripts, watch);
-gulp.task('default', development);
-
-// function runDevWebPack(sSource, sEntry) {
-// 	return gulp
-// 		.src(sSource)
-// 		.pipe(plumber())
-// 		.pipe(
-// 			webpack({
-// 				entry: {
-// 					main: sEntry,
-// 				},
-// 				mode: 'development',
-// 				output: {
-// 					filename: '[name].bundle.js',
-// 					// path: __dirname + '/js'
-// 				},
-// 				optimization: {
-// 					splitChunks: {
-// 						chunks: 'all',
-// 					},
-// 				},
-// 				devtool: 'source-map',
-// 				plugins: [
-// 					new MomentLocalesPlugin({
-// 						localesToKeep: ['uk'],
-// 					}),
-// 				],
-// 			})
-// 		)
-// 		.pipe(gulp.dest('js'))
-// 		.pipe(
-// 			browserSync.reload({
-// 				stream: true,
-// 			})
-// 		);
-// }
-
-// function runProdWebPack(sSource, sEntry, sDestination) {
-// 	return (
-// 		gulp
-// 			.src(sSource)
-// 			.pipe(plumber())
-// 			.pipe(
-// 				webpack({
-// 					entry: {
-// 						main: sEntry,
-// 					},
-// 					mode: 'production',
-// 					output: {
-// 						filename: '[name].bundle.js',
-// 					},
-// 					optimization: {
-// 						splitChunks: {
-// 							chunks: 'all',
-// 						},
-// 					},
-// 					plugins: [
-// 						new MomentLocalesPlugin({
-// 							localesToKeep: ['uk'],
-// 						}),
-// 					],
-// 				})
-// 			)
-// 			// .pipe(uglify({
-// 			// 	compress: {
-// 			// 		drop_console: true
-// 			// 	}
-// 			// }))
-// 			.pipe(gulp.dest(sDestination))
-// 	);
-// }
+gulp.task('build', gulp.series(clean, typeScripts, compress));
