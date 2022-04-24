68a9d548f56330c6235c990fb4a00c46e551868d
tougaj <sashamolodec@gmail.com>
2022-04-24 20:00:54 +0300

    Актуалізовано розділи новин

--- src/loader.ts
+++ src/loader.ts
@@ -72,14 +72,20 @@ const loadUkrNetNews = async ({ route, longTitle }: ISection) => {
 
 const loadAllNews = async () => {
 	const sections: ISection[] = [
-		{ route: 'main', longTitle: 'Головні події України та світу' },
+		{ route: 'russianaggression', longTitle: 'Війна РФ проти України' },
 		{ route: 'politics', longTitle: 'Політичні новини країни' },
 		{ route: 'economics', longTitle: 'Економіка та бізнес' },
-		{ route: 'covid19', longTitle: 'Коронавірус COVID-19' },
+		// { route: 'covid19', longTitle: 'Коронавірус COVID-19' },
 		{ route: 'criminal', longTitle: 'Оперативно про надзвичайні події' },
 		{ route: 'society', longTitle: 'Соціальні та культурні події' },
 		{ route: 'world', longTitle: 'Ситуація в світі' },
 		{ route: 'kyiv', longTitle: 'Події в Києві та області' },
+		{ route: 'dnipro', longTitle: 'Події в Дніпрі та області' },
+		{ route: 'zaporizhzhya', longTitle: 'Події в Запоріжжі та області' },
+		{ route: 'mikolayiv', longTitle: 'Події в Миколаєві та області' },
+		{ route: 'odesa', longTitle: 'Події в Одесі та області' },
+		{ route: 'kharkiv', longTitle: 'Події в Харкові та області' },
+		{ route: 'kherson', longTitle: 'Події в Херсоні та області' },
 		{ route: 'crimea', longTitle: 'Події в Криму' },
 		{ route: 'donetsk', longTitle: 'Події в Донецьку та області' },
 		{ route: 'luhansk', longTitle: 'Події в Луганську та області' },
